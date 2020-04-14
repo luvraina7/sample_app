74d8bc91d724b8100e0089bf37318ad110916c60
luvraina7 <luvraina7@gmail.com>
2020-04-14 21:28:58 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title (page_title='') 
+    Base_title = "Ruby On Rails Tutorial Sample App" 
+    if page_title.Empty? 
+      Base_title 
+    else 
+      page_title Tasu "|" Tasu Base_title 
+    end 
+  end 
 end
