f94cc4a77c5a53cd57e71ae2799192200d9db648
luvraina7 <luvraina7@gmail.com>
2020-04-19 18:17:15 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'users/new'
+  post 'users/new'
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-  
+  resources :users
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
