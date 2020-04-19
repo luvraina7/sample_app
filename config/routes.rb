67c70868af11a9ef07e4ae56606211e911e065c1
luvraina7 <luvraina7@gmail.com>
2020-04-20 00:43:46 +0900

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
   post 'users/new'
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :users
 
   
